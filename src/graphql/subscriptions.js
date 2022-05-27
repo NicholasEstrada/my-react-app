d9fd446598090e61e667814fc153d05adf51344a
NicholasEstrada <nicholas26cervatti@gmail.com>
2022-05-27 18:40:43 -0300

    primeiro commite

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateAuth = /* GraphQL */ `
+  subscription OnCreateAuth {
+    onCreateAuth {
+      id
+      email
+      password
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateAuth = /* GraphQL */ `
+  subscription OnUpdateAuth {
+    onUpdateAuth {
+      id
+      email
+      password
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteAuth = /* GraphQL */ `
+  subscription OnDeleteAuth {
+    onDeleteAuth {
+      id
+      email
+      password
+      createdAt
+      updatedAt
+    }
+  }
+`;
